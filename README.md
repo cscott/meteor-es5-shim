9b3c3792fa7ddfb4e0c645279b91e94c667e008a
C. Scott Ananian <cscott@cscott.net>
2014-12-09 11:03:45 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+meteor-es5-shim [![Build Status](https://travis-ci.org/cscott/meteor-es5-shim.svg?branch=master)](https://travis-ci.org/cscott/meteor-es5-shim)
+===============
+
+[es5-shim](https://github.com/es-shims/es5-shim) packaged for Meteor.
+
+Applies on just the client -- the server should be ES5-compatible already!
+
+Basic usage
+-----------
+```
+meteor add cscottnet:es5-shim
+```
---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'cscottnet:es5-shim',
+  summary: 'ES5 compatibility shims, packaged for Meteor',
+  version: '4.0.5',
+  git: 'https://github.com/cscott/meteor-es5-shim.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
+  api.addFiles('vendor/es5-shim.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use(['cscottnet:es5-shim','tinytest'], 'client');
+  api.add_files(['es5-shim-tests.js'], 'client');
+});
