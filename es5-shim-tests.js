2876922d8230bec779ba784ebc6342632332ef1c
C. Scott Ananian <cscott@cscott.net>
2016-01-10 23:23:43 -0500

    Fix typos in comments.

--- es5-shim-tests.js
+++ es5-shim-tests.js
@@ -1,6 +1,6 @@
 Tinytest.add('es5-shim - exists', function (test) {
   // es5-shim actually has a pretty-complete jasmine test suite,
-  // but haven't (yet?) sat down to figure out how to call it
+  // but I haven't (yet?) sat down to figure out how to call it
   // from Tinytest.
   test.isTrue('0b'.substr(-1) === 'b', 'es5-shim exists');
 });
--- thunk.js
+++ thunk.js
@@ -1,4 +1,4 @@
-// By default Meteor hints several globals from us.
+// By default Meteor hides several globals from us.
 // The below definitions are sufficient to expose them to es5-shim.
 Date = Date || window.Date || function() { /* gahhh */ };
 parseInt = parseInt || window.parseInt || function() { return 0; };
